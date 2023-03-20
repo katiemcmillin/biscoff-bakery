774475219603f68fbcae789fca59fc4a81e3c6a9
katiemcmillin <katiemcmillin@gmail.com>
2023-03-20 17:18:13 -0400

    added starter code

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const bakedGoods = require("./models/bakedGoods")
+
+app.get('/bakedGoods', (req, res) => {
+    res.send(bakedGoods)
+})
+
+app.get('/bakedGoods/:id', (req, res) => {
+    res.send(bakedGoods[req.params.id])
+})
+
+app.listen(PORT, () =>  console.log(`Listening on ${PORT}`))
