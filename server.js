2a60a86e3ce1a06790780372651bcae2cd5b40bf
fxhanloniii <hanlonfrankiex@gmail.com>
2023-03-21 19:24:55 -0700

    finished deliverable

--- server.js
+++ server.js
@@ -5,12 +5,30 @@ const bakedGoods = require("./models/bakedGoods")
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended:false }));
+
+
 app.get('/bakedGoods', (req, res) => {
-    res.send(bakedGoods)
-})
+    res.render('index', { bakedGoods });
+});
 
-app.get('/bakedGoods/:id', (req, res) => {
-    res.send(bakedGoods[req.params.id])
-})
 
-app.listen(PORT, () =>  console.log(`Listening on ${PORT}`))
+app.get('/bakedGoods/new', (req, res) => {
+    res.render('new');
+});
+
+app.get('/bakedGoods/:name', (req, res) => {
+    const name = req.params.name;
+    const bakedGood = bakedGoods.find(bg => bg.name === name);
+    res.render('show', { bakedGood });
+});
+
+
+
+app.post('/bakedGoods', (req, res) => {
+    const { name, price, image } = req.body;
+    bakedGoods.push({ name, price, image });
+    res.redirect('/bakedGoods');
+});
+
+app.listen(PORT, () =>  console.log(`Listening on ${PORT}`));
