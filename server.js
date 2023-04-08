78a491ea45eb1e7ee7cf8d1ca05e938112e0b592
katiemcmillin <katiemcmillin@gmail.com>
2023-04-08 15:03:16 -0400

    change server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const bakedGoods = require("./models/bakedGoods")
+console.log(app);
 
 app.get('/bakedGoods', (req, res) => {
     res.send(bakedGoods)
